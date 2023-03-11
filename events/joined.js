a8dee9e69c28ef3c21e4df9759e6c988170fc13a
OpTic SpiderAnt <68359008+OpTicSpiderAnt@users.noreply.github.com>
2023-03-11 15:02:59 -0500

    Update joined.js

--- events/joined.js
+++ events/joined.js
@@ -28,7 +28,7 @@ for (const [guildID, guild] of client.guilds.cache) {
     }
 
     invites.push({ name: guild.name, invite });
-}catch (error) {
+} catch (error) {
     const errorChannel = await message.client.channels.fetch('1060347445722230867')
     await errorChannel.send(`There has been an error! Here is the information sorrounding it.\n\nServer Found In: **Can't get Guild Name**\nUser Who Found It: **${message.author.tag}**・**${message.author.id}**\nFound Time: <t:${Math.trunc(Date.now() / 1000)}:R>\nThe Reason: **guildCreate event has an error**\nError: **${error.stack}**\n\`\`\` \`\`\``)
     console.log(error)
